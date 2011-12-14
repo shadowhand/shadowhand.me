43184096be8757cf0d3025b6e0c03a6013fbd14a
Woody Gilk <woody@wingsc.com>
2011-12-13 18:10:36 -0600

    Completed 2.0, beta version

--- application/classes/view/layout.php
+++ application/classes/view/layout.php
@@ -20,6 +20,62 @@ abstract class View_Layout extends Kostache_Layout {
 		);
 	}
 
+	public function social()
+	{
+		return array(
+			array(
+				'name'  => 'twitter',
+				'title' => 'Twitter',
+				'link'  => 'http://twitter.com/shadowhand',
+			),
+			array(
+				'name'  => 'google+',
+				'title' => 'Google+',
+				'link'  => 'http://plus.google.com/u/0/115222471649720738463/',
+			),
+			array(
+				'name'  => 'linkedin',
+				'title' => 'LinkedIn',
+				'link'  => 'http://www.linkedin.com/pub/woody-gilk/4/946/824',
+			),
+			array(
+				'name'  => 'flickr',
+				'title' => 'Flickr',
+				'link'  => 'http://www.flickr.com/photos/newood',
+			),
+			array(
+				'name'  => 'lastfm',
+				'title' => 'Last.fm',
+				'link'  => 'http://last.fm/user/woodygilk',
+			),
+			array(
+				'name'  => 'github',
+				'title' => 'Github',
+				'link'  => 'http://github.com/shadowhand',
+			),
+			array(
+				'name'  => 'aboutdotme',
+				'title' => 'About.me',
+				'link'  => 'http://about.me/shadowhand',
+			),
+			// array(
+			// 	'name'  => 'amazon',
+			// 	'title' => 'Amazon',
+			// 	'link'  => 'http://amzn.com/w/1474H3P2204L8',
+			// ),
+			array(
+				'name'  => 'google-talk',
+				'title' => 'Google Talk',
+				'link'  => 'gtalk:chat?jid=woody.gilk@gmail.com',
+			),
+			array(
+				'name'  => 'skype',
+				'title' => 'Skype',
+				'link'  => 'skype:woody.gilk?call',
+			),
+		);
+	}
+
 	public function menu()
 	{
 		$links = array(
